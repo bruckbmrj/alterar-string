d6f6d167572c1d578c90792017a99309cb9fc1a2
Bruno Braga <bruckbmrj@hotmail.com>
2021-04-28 23:01:38 -0300

    Resultado da atividade (com log)

---
+++ altera-string.js
@@ -0,0 +1,17 @@
+let email = document.querySelector('.email');
+let emailString = email.textContent = "bruno.braga@bbmprojects.com.br";
+
+let emailSeparado = emailString.split("@");
+console.log(emailSeparado);
+const emailPrefixo = emailSeparado[0];
+console.log(emailPrefixo);
+const prefixoTamanho = emailPrefixo.length;
+console.log(prefixoTamanho);
+const inicioPrefixo = emailPrefixo.substring(0,2);
+console.log(inicioPrefixo);
+const repeatString = '*'.repeat(prefixoTamanho - 2);
+console.log(repeatString);
+const emailModificado = `${inicioPrefixo}${repeatString}@${emailSeparado[1]}`;
+console.log(emailModificado);
+let resultado = email.textContent = emailModificado;
+console.log(resultado);
